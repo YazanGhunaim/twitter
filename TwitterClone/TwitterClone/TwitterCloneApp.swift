8107f046d4c70bd4f84f1a53fb4eed9abf871f6c
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-07-18 20:54:46 +0200

    firebase import

--- TwitterClone/TwitterClone/TwitterCloneApp.swift
+++ TwitterClone/TwitterClone/TwitterCloneApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterCloneApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
